23b5bc199f2f8e90ba94933e8dadcae6359a9a1f
David Horès <hores.d@gmail.com>
2026-02-16 15:03:21 +0100

    Create Employee Entity

---
+++ src/Entity/Employee.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+class Employee
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'id')]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'lastname', length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(name: 'firstname', length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(name: 'email', length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(name: 'start', type: Types::DATE_IMMUTABLE)]
+    private ?\DateTimeImmutable $start = null;
+
+    #[ORM\Column(name: 'status', length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getStart(): ?\DateTimeImmutable
+    {
+        return $this->start;
+    }
+
+    public function setStart(\DateTimeImmutable $start): static
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
