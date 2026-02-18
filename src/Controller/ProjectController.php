0bda5b5c002c7b61dce9c0b1bb2bea6f0871b6b9
David Horès <hores.d@gmail.com>
2026-02-18 16:13:29 +0100

    Add route and new method to ProjectController

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -24,6 +26,26 @@ final class ProjectController extends AbstractController
         return $this->render('project/index.html.twig', [
             'controller_name' => 'ProjectController',
             'projects' => $projects,
+        ]);
+    }
+
+    #[Route('/project/new', name: 'app_project_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($project);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('project/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
