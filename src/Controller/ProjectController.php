a47125fe25a997c6cdf6dd43b8a0c32802bf071b
David Horès <hores.d@gmail.com>
2026-02-18 10:56:34 +0100

    Create ProjectController

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProjectController extends AbstractController
+{
+    public function __construct(
+        private ProjectRepository $projectRepository,
+        private EntityManagerInterface $manager,
+    ) {}
+
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(): Response
+    {
+        $projects = $this->projectRepository->findAll();
+
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+            'projects' => $projects,
+        ]);
+    }
+}
