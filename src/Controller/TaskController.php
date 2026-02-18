2126f826dc449eb96ea3d8593d9f66b280da2c7b
David Horès <hores.d@gmail.com>
2026-02-19 00:53:45 +0100

    Add new method and update edit method of TaskController to avoid overlap between project and task ids

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -18,18 +18,35 @@ final class TaskController extends AbstractController
     ) {}
 
     #[Route('/project/{project}/task/{id}', name: 'app_task', requirements: ['project' => '\d+', 'id' => '\d+'], methods: ['GET', 'POST'])]
-    #[Route('/project/{project}/task/new', name: 'app_task_new', requirements: ['project' => '\d+'], methods: ['GET', 'POST'])]
-    public function edit(?Task $task, Project $project, Request $request): Response
+    public function edit(Task $task, Project $project, Request $request): Response
     {
-        if (!$task) {
-            $task = new Task();
-            $task->setProject($project);
-        } elseif ($task->getProject()->getId() !== $project->getId()) {
+        if ($task->getProject()->getId() !== $project->getId()) {
             throw $this->createNotFoundException('La tâche ne correspond pas au projet.');
         }
 
         $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+
+            return $this->redirectToRoute('app_project', ['id' => $project->getId()]);
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form,
+            'task' => $task,
+            'new' => false,
+        ]);
+    }
+
+    #[Route('/project/{project}/task/new', name: 'app_task_new', requirements: ['project' => '\d+'], methods: ['GET', 'POST'])]
+    public function new(Project $project, Request $request): Response
+    {
+        $task = new Task();
+        $task->setProject($project);
+
+        $form = $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -41,8 +58,7 @@ final class TaskController extends AbstractController
 
         return $this->render('task/edit.html.twig', [
             'form' => $form,
-            'task' => $task,
-            'new' => $task->getId() === null,
+            'new' => true,
         ]);
     }
 }
