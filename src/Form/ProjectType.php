72f7d443fba02e17bb4aac47d6d9713319a8d6ee
David Horès <hores.d@gmail.com>
2026-02-18 15:47:26 +0100

    Create ProjectType

---
+++ src/Form/ProjectType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use App\Entity\Project;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre du projet',
+            ])
+            ->add('employees', EntityType::class, [
+                'label' => 'Inviter des membres',
+                'class' => Employee::class,
+                'choice_label' => function (Employee $employee) {
+                    return $employee->getFirstname() . ' ' . $employee->getLastname();
+                },
+                'multiple' => true,
+                'required' => false,
+                'by_reference' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
