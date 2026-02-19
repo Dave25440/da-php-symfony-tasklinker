28880336362d00be020e23f2661fc97dfb8275eb
David Horès <hores.d@gmail.com>
2026-02-19 13:00:29 +0100

    Add route and edit method to EmployeeController

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Employee;
+use App\Form\EmployeeType;
 use App\Repository\EmployeeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,6 +25,28 @@ final class EmployeeController extends AbstractController
 
         return $this->render('employee/index.html.twig', [
             'employees' => $employees,
+        ]);
+    }
+
+    #[Route('/employee/{id}', name: 'app_employee_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function edit(?Employee $employee, Request $request): Response
+    {
+        if (!$employee) {
+            throw $this->createNotFoundException('Employé introuvable.');
+        }
+
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->flush();
+
+            return $this->redirectToRoute('app_employee');
+        }
+
+        return $this->render('employee/edit.html.twig', [
+            'form' => $form,
+            'employee' => $employee,
         ]);
     }
 }
